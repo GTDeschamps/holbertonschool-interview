ae711bdbc13f4f308ea2631bb2fe7f2763930ffd
GTDeschamps <g.deschamps@hotmail.fr>
2024-05-24 12:38:33 +0200

    betty styles

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -8,9 +8,10 @@
  */
 void swap_values(heap_t *a, heap_t *b)
 {
-    int temp = a->n;
-    a->n = b->n;
-    b->n = temp;
+	int temp = a->n;
+
+	a->n = b->n;
+	b->n = temp;
 }
 
 /**
@@ -19,33 +20,35 @@ void swap_values(heap_t *a, heap_t *b)
  */
 void bubble_up(heap_t *node)
 {
-    while (node->parent && node->n > node->parent->n)
-    {
-        swap_values(node, node->parent);
-        node = node->parent;
-    }
+	while (node->parent && node->n > node->parent->n)
+	{
+		swap_values(node, node->parent);
+		node = node->parent;
+	}
 }
 
 /**
- * find_insertion_point - Finds the insertion point for a new node in level order
+ * find_insertion_point -
+ * Finds the insertion point for a new node in level order
  * @root: Pointer to the root of the heap
  * Return: Pointer to the insertion point node
  */
 heap_t *find_insertion_point(heap_t *root)
 {
-    heap_t *queue[1024];
-    int front = 0, rear = 0;
+	heap_t *queue[1024];
+	int front = 0, rear = 0;
 
-    queue[rear++] = root;
-    while (front < rear)
-    {
-        heap_t *node = queue[front++];
-        if (!node->left || !node->right)
-            return node;
-        queue[rear++] = node->left;
-        queue[rear++] = node->right;
-    }
-    return NULL;
+	queue[rear++] = root;
+	while (front < rear)
+	{
+		heap_t *node = queue[front++];
+
+		if (!node->left || !node->right)
+			return (node);
+		queue[rear++] = node->left;
+		queue[rear++] = node->right;
+	}
+	return (NULL);
 }
 
 /**
@@ -56,36 +59,36 @@ heap_t *find_insertion_point(heap_t *root)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *insert_point;
-    heap_t *new_node;
+	heap_t *insert_point;
+	heap_t *new_node;
 
-    if (!root)
-        return NULL;
+	if (!root)
+		return (NULL);
 
-    if (!*root)
-    {
-        *root = binary_tree_node(NULL, value);
-        return *root;
-    }
+	if (!*root)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
 
-    insert_point = find_insertion_point(*root);
+	insert_point = find_insertion_point(*root);
 
-    if (!insert_point)
-    {
-        *root = binary_tree_node(NULL, value);
-        return *root;
-    }
+	if (!insert_point)
+	{
+		*root = binary_tree_node(NULL, value);
+		return (*root);
+	}
 
-    new_node = binary_tree_node(insert_point, value);
+	new_node = binary_tree_node(insert_point, value);
 
-    if (!new_node)
-        return NULL;
+	if (!new_node)
+		return (NULL);
 
-    if (!insert_point->left)
-        insert_point->left = new_node;
-    else
-        insert_point->right = new_node;
+	if (!insert_point->left)
+		insert_point->left = new_node;
+	else
+		insert_point->right = new_node;
 
-    bubble_up(new_node);
-    return new_node;
+	bubble_up(new_node);
+	return (new_node);
 }
--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -13,14 +13,13 @@
  */
 typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 } binary_tree_t;
 
 typedef struct binary_tree_s heap_t;
-
 
 heap_t *binary_tree_node(heap_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
