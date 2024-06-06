c2b2360900c59e038be88092ec830063eaff6b9a
GTDeschamps <g.deschamps@hotmail.fr>
2024-06-06 14:40:36 +0200

    correction for all cases

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -24,26 +24,14 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 		for (j = 0; j < 3; j++)
 		{
 			sum = grid1[i][j] + grid2[i][j];
-			grid1[i][j] = sum % 4;
-			toppling = sum / 4;
-
-			for (k = 0; k < 3; k++)
-			{
-				for (l = 0; l < 3; l++)
-				{
-					if (k == i && l == j)
-					{
-						continue;
-					}
-					grid1[k][l] += toppling;
-				}
-			}
+			grid1[i][j] = sum;
 		}
 	}
 
 	while (1)
 	{
 		toppling = 0;
+		int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
 		for (i = 0; i < 3; i++)
 		{
 			for (j = 0; j < 3; j++)
@@ -51,15 +39,19 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 				if (grid1[i][j] > 3)
 				{
 					toppling++;
-					grid1[i][j] -= 4;
+					temp[i][j] = grid1[i][j] - 4;
 					if (i > 0)
-						grid1[i - 1][j]++;
+						temp[i - 1][j]++;
 					if (i < 2)
-						grid1[i + 1][j]++;
+						temp[i + 1][j]++;
 					if (j > 0)
-						grid1[i][j - 1]++;
+						temp[i][j - 1]++;
 					if (j < 2)
-						grid1[i][j + 1]++;
+						temp[i][j + 1]++;
+				}
+				else
+				{
+					temp[i][j] = grid1[i][j];
 				}
 			}
 		}
@@ -68,6 +60,13 @@ void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 			break;
 		}
 		printf("=\n");
-		print_grid(grid1);
+		print_grid(temp);
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid1[i][j] = temp[i][j];
+			}
+		}
 	}
 }
