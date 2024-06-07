fbe35617d7c0c3bb4c6040a719ffe79c9ecf5aba
GTDeschamps <g.deschamps@hotmail.fr>
2024-06-07 14:28:54 +0200

    Correction  finale

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -14,11 +14,73 @@ void print_grid(int grid[3][3])
 	{
 		for (j = 0; j < 3; j++)
 		{
+			if (j)
+				printf(" ");
 			printf("%d ", grid[i][j]);
 		}
 		printf("\n");
 	}
 }
+
+/**
+ * unstable - checks if the grid is unstable
+ * return 1 if unstable, 0 if stable
+ */
+int is_unstable(int grid[3][3])
+{
+	int i, j;
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			if (grid[i][j] > 3)
+			return (1);
+		}
+	}
+	return (0);
+}
+
+
+
+void topple(int grid[3][3])
+{
+	int i, j;
+
+	int temp[3][3] = {{0}};
+
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (grid[i][j] > 3)
+				{
+					temp[i][j] -= 4;
+
+					if (i > 0)
+						temp[i - 1][j]++;
+
+					if (i < 2)
+						temp[i + 1][j]++;
+
+					if (j > 0)
+						temp[i][j - 1]++;
+
+					if (j < 2)
+						temp[i][j + 1]++;
+				}
+			}
+		}
+
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				grid[i][j] += temp[i][j];
+			}
+		}
+}
+
 
 /**
  * sandpiles_sum - Computes the sum of two sandpiles
@@ -27,56 +89,20 @@ void print_grid(int grid[3][3])
  */
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
 {
-	int i, j, sum, toppling;
-	int temp[3][3] = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+	int i, j;
 
 	for (i = 0; i < 3; i++)
 	{
 		for (j = 0; j < 3; j++)
 		{
-			sum = grid1[i][j] + grid2[i][j];
-			grid1[i][j] = sum;
+			grid1[i][j] += grid2[i][j];
 		}
 	}
 
-	while (1)
+	while (is_unstable(grid1))
 	{
-		toppling = 0;
-		
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				if (grid1[i][j] > 3)
-				{
-					toppling++;
-					temp[i][j] = grid1[i][j] - 4;
-					if (i > 0) temp[i - 1][j]++;
-					if (i < 2) temp[i + 1][j]++;
-					if (j > 0) temp[i][j - 1]++;
-					if (j < 2) temp[i][j + 1]++;
-				}
-				else
-				{
-					temp[i][j] = grid1[i][j];
-				}
-			}
-		}
-
-		if (toppling == 0)
-		{
-			break;
-		}
-
 		printf("=\n");
-		print_grid(temp);
-
-		for (i = 0; i < 3; i++)
-		{
-			for (j = 0; j < 3; j++)
-			{
-				grid1[i][j] = temp[i][j];
-			}
-		}
+		print_grid(grid1);
+		topple(grid1);
 	}
 }
