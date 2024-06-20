480658c3509dcbfdd62303f4dcaedd1ff3c7b5b5
GTDeschamps <g.deschamps@hotmail.fr>
2024-06-20 16:10:37 +0200

    correction step by step

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -40,7 +40,8 @@ def print_status():
     print("File size:", total_size)
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
-            print(code, ":", status_codes[code])
+            print(code,end="")
+            print(":", status_codes[code])
 
     # Iterate over input lines from stdin
 for line in lines:
