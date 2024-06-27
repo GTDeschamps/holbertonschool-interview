0ea224c9f5f26af4439bb6d0a8a757bc01ea1922
GTDeschamps <g.deschamps@hotmail.fr>
2024-06-27 10:49:10 +0200

    Interview: Check cycle

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+    listint_t *slow = list;
+    listint_t *fast = list;
+
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast)
+            return 1; /* cycle detected */
+    }
+
+    return 0; /* no cycle */
+}
