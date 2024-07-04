119afaf3fc1b9a168958b9598a0e59dfd34df226
GTDeschamps <g.deschamps@hotmail.fr>
2024-07-04 11:42:37 +0200

    Interview : Palindrome integer

---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,12 @@
+#ifndef PALINDROME_H
+#define PALINDROME_H
+
+/**
+ * is_palindrome - Check if a given unsigned integer is a palindrome.
+ * @n: The number to be checked.
+ *
+ * Return: 1 if n is a palindrome, 0 otherwise.
+ */
+int is_palindrome(unsigned long n);
+
+#endif /* PALINDROME_H */
