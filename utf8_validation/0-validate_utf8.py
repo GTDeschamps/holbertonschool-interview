1c72c75bb88a5d2baec5adc79e81ad031e5ab440
GTDeschamps <g.deschamps@hotmail.fr>
2024-07-10 10:21:36 +0200

    documentation PEP8 style

--- utf8_validation/0-validate_utf8.py
+++ utf8_validation/0-validate_utf8.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def validUTF8(data):
+def valid_utf8(data):
     """
     Check if a given data set represents a valid UTF-8 encoding.
 
@@ -10,8 +10,7 @@ def validUTF8(data):
     Returns:
     bool: True if data is a valid UTF-8 encoding, else False
     """
-
-    num_bytes = 0   # Counter to track the number of bytes in a UTF-8 character
+    num_bytes = 0  # Counter to track the number of bytes in a UTF-8 character
 
     for byte in data:
         byte = bin(byte)[2:].zfill(8)  # Convert byte to binary and pad with zeros to 8 bits
@@ -27,10 +26,10 @@ def validUTF8(data):
             elif byte.startswith('11110'):
                 num_bytes = 3
             else:
-                return False    # Invalid UTF-8 encoding found
+                return False  # Invalid UTF-8 encoding found
         else:
             if not byte.startswith('10'):
-                return False     # Invalid continuation byte
+                return False  # Invalid continuation byte
             num_bytes -= 1  # Decrease the remaining bytes to process
 
-    return num_bytes == 0   # Check if all character bytes have been processed
+    return num_bytes == 0  # Check if all character bytes have been processed
