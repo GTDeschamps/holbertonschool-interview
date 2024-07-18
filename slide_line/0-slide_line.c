729f3acea43e0255a0e498e53de589ac6a56761b
GTDeschamps <g.deschamps@hotmail.fr>
2024-07-18 10:59:04 +0200

    ISO C90 for Slide_line

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -12,87 +12,106 @@
  *
  * @return 1 upon success, 0 upon failure.
  */
-int slide_line(int *line, size_t size, int direction) {
-    if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT) {
-        return 0; // Failure: invalid direction
-    }
+int slide_line(int *line, size_t size, int direction)
+{
+	if (direction != SLIDE_LEFT && direction != SLIDE_RIGHT)
+	{
+		/* Failure: invalid direction */
+		return 0;
+	}
 
-    size_t i, j;
-    int temp;
+	size_t i, j;
+	int temp;
 
-    if (direction == SLIDE_LEFT) {
-        // Remove zeros from the left
-        i = 0;
-        j = 0;
-        while (j < size) {
-            if (line[j] != 0) {
-                temp = line[i];
-                line[i] = line[j];
-                line[j] = temp;
-                i++;
-            }
-            j++;
-        }
+	if (direction == SLIDE_LEFT)
+	{
+		/* Remove zeros from the left */
+		i = 0;
+		j = 0;
+		while (j < size)
+		{
+			if (line[j] != 0)
+			{
+				temp = line[i];
+				line[i] = line[j];
+				line[j] = temp;
+				i++;
+			}
+			j++;
+		}
 
-        // Merge identical numbers
-        i = 0;
-        while (i < size - 1) {
-            if (line[i] == line[i + 1]) {
-                line[i] *= 2;
-                line[i + 1] = 0;
-            }
-            i++;
-        }
+		/* Merge identical numbers */
+		i = 0;
+		while (i < size - 1)
+		{
+			if (line[i] == line[i + 1])
+			{
+				line[i] *= 2;
+				line[i + 1] = 0;
+			}
+			i++;
+		}
 
-        // Remove zeros from the left again
-        i = 0;
-        j = 0;
-        while (j < size) {
-            if (line[j] != 0) {
-                temp = line[i];
-                line[i] = line[j];
-                line[j] = temp;
-                i++;
-            }
-            j++;
-        }
-    } else {
-        // Remove zeros from the right
-        i = size - 1;
-        j = size - 1;
-        while (j > 0) {
-            if (line[j] != 0) {
-                temp = line[i];
-                line[i] = line[j];
-                line[j] = temp;
-                i--;
-            }
-            j--;
-        }
+		/* Remove zeros from the left again */
+		i = 0;
+		j = 0;
+		while (j < size)
+		{
+			if (line[j] != 0)
+			{
+				temp = line[i];
+				line[i] = line[j];
+				line[j] = temp;
+				i++;
+			}
+			j++;
+		}
+	}
+	else
+	{
+		/* Remove zeros from the right */
+		i = size - 1;
+		j = size - 1;
+		while (j > 0)
+		{
+			if (line[j] != 0)
+			{
+				temp = line[i];
+				line[i] = line[j];
+				line[j] = temp;
+				i--;
+			}
+			j--;
+		}
 
-        // Merge identical numbers
-        i = size - 1;
-        while (i > 0) {
-            if (line[i] == line[i - 1]) {
-                line[i] *= 2;
-                line[i - 1] = 0;
-            }
-            i--;
-        }
+		/* Merge identical numbers */
+		i = size - 1;
+		while (i > 0)
+		{
+			if (line[i] == line[i - 1])
+			{
+				line[i] *= 2;
+				line[i - 1] = 0;
+			}
+			i--;
+		}
 
-        // Remove zeros from the right again
-        i = size - 1;
-        j = size - 1;
-        while (j > 0) {
-            if (line[j] != 0) {
-                temp = line[i];
-                line[i] = line[j];
-                line[j] = temp;
-                i--;
-            }
-            j--;
-        }
-    }
+		/* Remove zeros from the right again */
+		i = size - 1;
+		j = size - 1;
+		while (j > 0)
+		{
+			if (line[j] != 0)
+			{
+				temp = line[i];
+				line[i] = line[j];
+				line[j] = temp;
+				i--;
+			}
+			j--;
+		}
+	}
 
-    return 1; // Success
+	/* Success */
+	return 1;
 }
