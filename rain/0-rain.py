0a511a31cab59a0eb1d1d01685e8110f191a0ed3
GTDeschamps <g.deschamps@hotmail.fr>
2024-08-27 10:15:19 +0200

    add documentation

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
 
 def rain(walls):
+    """
+    Calculate how many square units of water will be retained after it rains.
+
+    Args:
+        walls (List[int]): A list of non-negative integers representing the heights of walls.
+
+    Returns:
+        int: Total amount of rainwater retained. Returns 0 if the list is empty.
+    """
     # Check if the list is empty
     if not walls:
         return 0
