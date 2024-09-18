81a16987a75855bf0aeccb8551de730f1d995128
GTDeschamps <g.deschamps@hotmail.fr>
2024-09-18 10:16:32 +0200

    change the order of import

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -4,9 +4,9 @@ create recursive fonction that queries the Reddit API,
 parses the title of all hot articles,
 and prints a sorted count of given keywords.
 '''
+from collections import Counter
 import re
 import requests
-from collections import Counter
 
 
 def count_words(subreddit, word_list, after=None, count={}):
