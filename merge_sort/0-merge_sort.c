a4e0da46217c3e20b84e6a198eb2020b9bc9bf4d
GTDeschamps <g.deschamps@hotmail.fr>
2024-10-17 13:33:04 +0200

    update and correction of code

--- merge_sort/0-merge_sort.c
+++ merge_sort/0-merge_sort.c
@@ -3,55 +3,16 @@
 #include "sort.h"
 
 /**
- * print_sub_array - Prints part of an array (from left to right)
- * with a comma-separated format.
+ * merge_sort_rec - Recursively divides and sorts the array using merge sort.
  */
-void print_sub_array(const int *array, size_t left, size_t right)
-{
-    for (size_t i = left; i <= right; i++)
-    {
-        if (i > left)
-            printf(", ");
-        printf("%d", array[i]);
-    }
-    printf("\n");
-}
+void merge_sort_rec(int *array, int *temp, int left, int right);
 
 /**
- * merge - Merges two sub-arrays of array[].
+ * merge - Merges two arrays of array[].
  * First sub-array is arr[left..mid]
  * Second sub-array is arr[mid+1..right]
  */
-void merge(int *array, int *temp, size_t left, size_t mid, size_t right)
-{
-    size_t i = left, j = mid + 1, k = left;
-
-    printf("Merging...\n");
-    printf("[left]: ");
-    print_sub_array(array, left, mid);
-    printf("[right]: ");
-    print_sub_array(array, mid + 1, right);
-
-    while (i <= mid && j <= right)
-    {
-        if (array[i] <= array[j])
-            temp[k++] = array[i++];
-        else
-            temp[k++] = array[j++];
-    }
-
-    while (i <= mid)
-        temp[k++] = array[i++];
-
-    while (j <= right)
-        temp[k++] = array[j++];
-
-    for (i = left; i <= right; i++)
-        array[i] = temp[i];
-
-    printf("[Done]: ");
-    print_sub_array(array, left, right);
-}
+void merge(int *array, int left, int mid, int right, int *temp);
 
 /**
  * merge_sort - The top-level function that starts merge sort.
@@ -73,12 +34,12 @@ void merge_sort(int *array, size_t size)
 /**
  * merge_sort_rec - Recursively divides and sorts the array using merge sort.
  */
-void merge_sort_rec(int *array, int *temp, size_t left, size_t right)
+void merge_sort_rec(int *array, int *temp, int left, int right)
 {
     if (left >= right)
         return;
 
-    size_t mid = left + (right - left) / 2;
+    int mid = left + (right - left) / 2;
 
     // Sort the left part
     merge_sort_rec(array, temp, left, mid);
@@ -87,6 +48,39 @@ void merge_sort_rec(int *array, int *temp, size_t left, size_t right)
     merge_sort_rec(array, temp, mid + 1, right);
 
     // Merge the sorted parts
-    merge(array, temp, left, mid, right);
+    merge(array, left, mid, right, temp);
 }
 
+/**
+ * merge - Merges two arrays of array[].
+ * First sub-array is arr[left..mid]
+ * Second sub-array is arr[mid+1..right]
+ */
+void merge(int *array, int left, int mid, int right, int *temp)
+{
+    int i = left, j = mid + 1, k = left;
+
+    printf("Merging...\n");
+    print_array(array + left, mid - left + 1);
+    print_array(array + mid + 1, right - mid);
+
+    while (i <= mid && j <= right)
+    {
+        if (array[i] <= array[j])
+            temp[k++] = array[i++];
+        else
+            temp[k++] = array[j++];
+    }
+
+    while (i <= mid)
+        temp[k++] = array[i++];
+
+    while (j <= right)
+        temp[k++] = array[j++];
+
+    for (i = left; i <= right; i++)
+        array[i] = temp[i];
+
+    printf("[Done]: ");
+    print_array(array + left, right - left + 1);
+}
--- merge_sort/sort.h
+++ merge_sort/sort.h
@@ -1,12 +1,17 @@
 #ifndef SORT_H
 #define SORT_H
 
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
  * print_array - Prints an array of integers.
  * @array: The array to be printed.
  * @size: The size of the array.
  */
-void print_array(int *array, size_t size);
+void print_array( int *array, size_t size);
 void merge_sort(int *array, size_t size);
+void merge(int *array, int left, int mid, int right, int *temp);
 
 #endif /* SORT_H */
