5fdbbf9a09ec000824348db7eb076bf21910c92f
GTDeschamps <g.deschamps@hotmail.fr>
2024-10-21 14:52:12 +0200

    PEP8 style

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -4,11 +4,13 @@ function to determine the fewest number of coins needed
 to meet a given amount total.
 """
 
+
 def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize the dp array where dp[i] is the minimum number of coins need to make i
+    # Initialize the dp array where dp[i] is the minimum number of coins
+    # need to make i.
     dp = [float('inf')]*(total + 1)
     dp[0] = 0 # 0 coins are needed to make 0
 
@@ -19,5 +21,6 @@ def makeChange(coins, total):
             if i - coin >= 0:
                 dp[i] = min(dp[i], dp[i - coin]+1)
 
-    # if dp[total] is still infinity, it means we cannot make the total with the given coins
+    # if dp[total] is still infinity, it means we cannot make the total
+    # with the given coins.
     return dp[total] if dp[total] != float('inf') else -1
