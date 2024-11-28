9da6d05c007abaed723f0d9dce6f6f61d7a4bcf2
GTDeschamps <g.deschamps@hotmail.fr>
2024-11-28 16:36:48 +0100

    betty style

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -11,31 +11,31 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    // Base cases
-    if (*s1 == '\0' && *s2 == '\0')
-    {
-        return 1; // Both strings are empty
-    }
-    if (*s2 == '*' && *(s2 + 1) == '\0')
-    {
-        return 1; // '*' at the end of s2 matches anything
-    }
+	/*Base cases*/
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return 1; // Both strings are empty
+	}
+	if (*s2 == '*' && *(s2 + 1) == '\0')
+	{
+		return 1; // '*' at the end of s2 matches anything
+	}
 
-    // If the current characters match or s2 has a '*'
-    if (*s1 == *s2 || *s2 == '*')
-    {
-        // If s2 has a '*', try matching the current character or skipping it
-        if (*s2 == '*')
-        {
-            return wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
-        }
-        else
-        {
-            // Move to the next character in both strings
-            return wildcmp(s1 + 1, s2 + 1);
-        }
-    }
+	/*If the current characters match or s2 has a '*' */
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		/*If s2 has a '*', try matching the current character or skipping it*/
+		if (*s2 == '*')
+		{
+			return wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2));
+		}
+		else
+		{
+			/*Move to the next character in both strings*/
+			return wildcmp(s1 + 1, s2 + 1);
+		}
+	}
 
-    // If the current characters don't match and s2 doesn't have a '*'
-    return 0;
+	/*If the current characters don't match and s2 doesn't have a '*'*/
+	return 0;
 }
