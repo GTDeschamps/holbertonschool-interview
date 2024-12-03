a7fae43f6b9063482dab5e6940e470fefc4412ab
GTDeschamps <g.deschamps@hotmail.fr>
2024-12-03 16:45:48 +0100

    documentation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
-
 def pascal_triangle(n):
     """
     This function returns a list of lists of integers
     representing Pascal's triangle of n.
 
     Args:
-    n: An integer representing the number of rows in the Pascal's triangle.
+        n (int): The number of rows in Pascal's triangle to generate.
+                Must be a non-negative integer.
 
     Returns:
-        A list of lists of integers representing Pascal's triangle.
+        List[List[int]]: A list of lists, where each inner list represents
+                        a row in Pascal's triangle. The outer list contains
+                        all the rows generated.
+                        
+    Example:
+        If n = 5, the function will return:
+        [
+            [1],
+            [1, 1],
+            [1, 2, 1],
+            [1, 3, 3, 1],
+            [1, 4, 6, 4, 1]
+        ]
     """
 
     # Check if the input is less than or equal to 0
