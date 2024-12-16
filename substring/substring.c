ed270f896393df0d83161f864126ee0b4d4b8e4c
GTDeschamps <g.deschamps@hotmail.fr>
2024-12-16 16:44:31 +0100

    betty

--- substring/substring.c
+++ substring/substring.c
@@ -3,14 +3,17 @@
 #include <stdio.h>
 
 /**
- * find_substring - Finds all starting indices of substrings that are a concatenation
+ * find_substring - Finds all starting indices of substrings
+ * 					that are a concatenation
  *                  of a list of words in a given string.
  * @s: The string to scan.
  * @words: The array of words all substrings must be a concatenation of.
  * @nb_words: The number of elements in the array words.
- * @n: A pointer to an integer where the function will store the number of results.
+ * @n: A pointer to an integer where the function will store
+ * the number of results.
  *
- * Return: An allocated array containing each index in `s` where a valid substring starts.
+ * Return: An allocated array containing each index in `s`
+ * 			where a valid substring starts.
  *         If no valid substring is found, NULL is returned.
  */
 int *find_substring(char const *s, char const **words, int nb_words, int *n)
@@ -19,14 +22,14 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 	int *word_count = NULL, *current_count = NULL;
 
 	if (!s || !words || nb_words == 0 || !n)
-		return NULL;
+		return (NULL);
 
 	word_len = strlen(words[0]);
 	s_len = strlen(s);
 	substr_len = word_len * nb_words;
 
 	if (substr_len > s_len)
-		return NULL;
+		return (NULL);
 
 	word_count = calloc(nb_words, sizeof(int));
 	current_count = calloc(nb_words, sizeof(int));
@@ -37,7 +40,7 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 		free(word_count);
 		free(current_count);
 		free(indices);
-		return NULL;
+		return (NULL);
 	}
 
 	/* Count occurrences of each word in the array `words`*/
@@ -88,9 +91,9 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 	{
 		free(indices);
 		*n = 0;
-		return NULL;
+		return (NULL);
 	}
 
 	*n = count;
-	return realloc(indices, count * sizeof(int));
+	return (realloc(indices, count * sizeof(int)));
 }
